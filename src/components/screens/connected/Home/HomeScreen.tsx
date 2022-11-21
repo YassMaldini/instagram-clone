30828a6d64d3c58baa2741f7974470b91e43f2c1
YassMaldini <yass.maldini@gmail.com>
2022-11-21 09:01:36 +0100

    using axios instead of apisauce

--- src/components/screens/connected/Home/HomeScreen.tsx
+++ src/components/screens/connected/Home/HomeScreen.tsx
@@ -2,43 +2,44 @@ import { useMemo } from 'react';
 import HomeHeader from './HomeHeader/HomeHeader';
 import Box from '../../../designSystem/Box/Box';
 import TimelineFeed from '../../../commons/TimelineFeed/TimelineFeed';
-import { timelineSample } from '../../../../utils/api/samples/timelineSample';
-import { reelsTraySample } from '../../../../utils/api/samples/reelsTraySample';
 import { HomeScreenContextProps } from './HomeScreen.types';
 import { HomeContext } from './HomeScreen.context';
+import useTimelineFeed from '../../../../hooks/feed/useTimelineFeed/useTimelineFeed';
+import useReelsTrayFeed from '../../../../hooks/feed/useReelsTrayFeed/useReelsTrayFeed';
+import { useMergedStatesFromFetchers } from '../../../../hooks/useMergedStatesFromFetchers/useMergedStatesFromFetchers';
 
 const HomeScreen = () => {
-  // const {
-  //   data: timeline,
-  //   isLoading: isTimelineLoading,
-  //   error: timelineError
-  // } = useTimelineFeed();
+  const {
+    data: timeline,
+    isLoading: isTimelineLoading,
+    error: timelineError
+  } = useTimelineFeed();
 
-  // const {
-  //   data: reelsTray,
-  //   isLoading: isReelsTrayLoading,
-  //   error: reelsTrayError
-  // } = useReelsTrayFeed();
+  const {
+    data: reelsTray,
+    isLoading: isReelsTrayLoading,
+    error: reelsTrayError
+  } = useReelsTrayFeed();
 
-  // const { isLoading, error } = useMergedStatesFromFetchers({
-  //   loadings: [isTimelineLoading, isReelsTrayLoading,],
-  //   errors: [timelineError, reelsTrayError]
-  // });
+  const { isLoading, error } = useMergedStatesFromFetchers({
+    loadings: [isTimelineLoading, isReelsTrayLoading,],
+    errors: [timelineError, reelsTrayError]
+  });
 
   const contextValue = useMemo<HomeScreenContextProps>(
     () => ({
-      timeline: timelineSample,
-      reelsTray: reelsTraySample,
+      timeline,
+      reelsTray,
       isLoading: false,
       error: null,
     }),
-    // [
-    //   timeline,
-    //   reelsTray,
-    //   isLoading,
-    //   error
-    // ]
-    [timelineSample, reelsTraySample]
+    [
+      timeline,
+      reelsTray,
+      isLoading,
+      error
+    ]
+    // [timelineSample, reelsTraySample]
   );
 
   return (
